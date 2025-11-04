fca065a19371b7050d570661d90d252fddd482e6
Leesuhwan1445 <lhh080@naver.com>
2025-11-04 16:59:36 +0900

    Spring boot 프로젝트 생성 및 깃이그노어 추가

---
+++ src/main/java/com/teamgreen/makeplan/server/MakeplanServerApplication.java
@@ -0,0 +1,13 @@
+package com.teamgreen.makeplan.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeplanServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeplanServerApplication.class, args);
+	}
+
+}
