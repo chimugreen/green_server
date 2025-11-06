96a84011a30cdb802474b08d6b38c71f4afe7e18
Leesuhwan1445 <lhh080@naver.com>
2025-11-06 13:00:24 +0900

    Todo Entiry 생성 및 repository 생성

--- src/main/java/com/teamgreen/makeplan/server/entity/Todo.java
+++ src/main/java/com/teamgreen/makeplan/server/entity/Todo.java
@@ -1,4 +1,29 @@
 package com.teamgreen.makeplan.server.entity;
 
-public class ToDo {
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="todo")
+@Data
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id; //글번호
+
+    @Column(name = "is_done", nullable = false, columnDefinition = "TINYINT(1) default 0")
+    private boolean isDone; // todo 완료 / 미완료 기본값 미완료
+
+    @Column(nullable = false)
+    private String content; // todo 내용
+
+    private LocalDateTime targetDate; //언제까지 완료해야되는지
+
+    @ManyToOne
+    @JoinColumn(name = "writer_Id")
+    private User writer; //작성자
+
 }
--- src/main/java/com/teamgreen/makeplan/server/repository/TodoRepository.java
+++ src/main/java/com/teamgreen/makeplan/server/repository/TodoRepository.java
@@ -1,4 +1,9 @@
 package com.teamgreen.makeplan.server.repository;
 
-public class TodoRepository {
+
+import com.teamgreen.makeplan.server.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo, Integer> {
+
 }
