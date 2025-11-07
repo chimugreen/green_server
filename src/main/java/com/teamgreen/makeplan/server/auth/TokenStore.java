1506c47c308fa5fe8c43ffbfc5b81098ae8e76cb
HyeonJun Kang <khjj9865@naver.com>
2025-11-07 10:18:04 +0900

    add: TokenStore

---
+++ src/main/java/com/teamgreen/makeplan/server/auth/TokenStore.java
@@ -0,0 +1,27 @@
+package com.teamgreen.makeplan.server.auth;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class TokenStore {
+    private static Map<String, String> tokenMap = new ConcurrentHashMap<>();
+
+    public void save(String email, String token) {
+        tokenMap.put(email, token);
+    }
+
+    public String find(String email) {
+        return tokenMap.get(email);
+    }
+
+    public void delete(String email) {
+        tokenMap.remove(email);
+    }
+
+    public boolean exists(String email, String token) {
+        return token.equals(tokenMap.get(email));
+    }
+}
