28375456c10bb30062f718986d18865e24281bae
HyeonJun Kang <khjj9865@naver.com>
2025-11-07 11:33:22 +0900

    chore: 생성자 추가

--- src/main/java/com/teamgreen/makeplan/server/dto/user/SignUpReqDto.java
+++ src/main/java/com/teamgreen/makeplan/server/dto/user/SignUpReqDto.java
@@ -2,16 +2,18 @@ package com.teamgreen.makeplan.server.dto.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class SignUpReqDto {
 
     @NotEmpty(message = "사용자 email은 필수항목입니다.")
     @Email(message = "올바른 email형식이 아닙니다.")
-    private String email;
+    private final String email;
 
     @NotEmpty(message = "비밀번호는 필수 항목입니다")
-    private String password;
+    private final String password;
 
 }
