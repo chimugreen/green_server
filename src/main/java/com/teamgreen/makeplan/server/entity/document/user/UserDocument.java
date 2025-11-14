9dd6104a0d5bc40356c04239c43c84c6b9af540e
HyeonJun Kang <khjj9865@naver.com>
2025-11-14 12:02:49 +0900

    add: MongoDB에 user document 추가

---
+++ src/main/java/com/teamgreen/makeplan/server/entity/document/user/UserDocument.java
@@ -0,0 +1,26 @@
+package com.teamgreen.makeplan.server.entity.document.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(collection = "user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDocument {
+    @Id
+    private String mongoId; // Mongo의 내부 id
+    private Integer id; //Rdb의 userID
+    @Builder.Default
+    private List<Integer> followers = new ArrayList<>();
+    @Builder.Default
+    private List<Integer> following = new ArrayList<>();
+}
