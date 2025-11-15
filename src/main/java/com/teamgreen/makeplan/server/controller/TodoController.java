76a9550da6f4c90a70a899fa4115989a1141b2de
Leesuhwan1445 <lhh080@naver.com>
2025-11-15 15:19:34 +0900

    tokenStore의 변경에 맞춰서 Controller 수정

--- src/main/java/com/teamgreen/makeplan/server/controller/TodoController.java
+++ src/main/java/com/teamgreen/makeplan/server/controller/TodoController.java
@@ -1,5 +1,6 @@
 package com.teamgreen.makeplan.server.controller;
 
+import com.teamgreen.makeplan.server.auth.JwtTokenProvider;
 import com.teamgreen.makeplan.server.auth.TokenStore;
 import com.teamgreen.makeplan.server.dto.todo.TodoReqDto;
 import com.teamgreen.makeplan.server.dto.todo.CreateTodoResDto;
@@ -16,43 +17,47 @@ public class TodoController {
 
         private final TodoService todoService;
         private final TokenStore tokenStore;
+        private final JwtTokenProvider jwtTokenProvider;
 
         // Todo 생성
         @PostMapping
-        public CreateTodoResDto createTodo(
-                @RequestBody TodoReqDto todoReqDto,
-                @RequestHeader("Authorization") String authorizationHeader
-        ) {
-                // 🔥 Authorization: "Bearer eyJxxxxx"
-                String accessToken = authorizationHeader.replace("Bearer ", "").trim();
+        public CreateTodoResDto createTodo(@RequestBody TodoReqDto todoReqDto, @RequestHeader("Authorization") String authorizationHeader) {
 
-                String email = tokenStore.getEmailByToken(accessToken);
+//                String accessToken = authorizationHeader.replace("Bearer ", "").trim();
+//
+//                String email = tokenStore.getEmailByToken(accessToken);
+
+                String token = authorizationHeader.replace("Bearer ","").trim();
+
+                String email = jwtTokenProvider.extractEmail(token);
 
                 return todoService.createTodo(todoReqDto.getContent(), email);
         }
 
         // Todo 목록 조회 (로그인한 유저만)
         @GetMapping
-        public List<CreateTodoResDto> getTodosByEmail(
-                @RequestHeader("Authorization") String authorizationHeader
-        ) {
-                String accessToken = authorizationHeader.replace("Bearer ", "").trim();
-                String email = tokenStore.getEmailByToken(accessToken);
+        public List<CreateTodoResDto> getTodosByEmail(@RequestHeader("Authorization") String authorizationHeader) {
+//                String accessToken = authorizationHeader.replace("Bearer ", "").trim();
+//                String email = tokenStore.getEmailByToken(accessToken);
+                String token = authorizationHeader.replace("Bearer ","").trim();
+                String email = jwtTokenProvider.extractEmail(token);
 
                 return todoService.getTodosByEmail(email);
         }
 
         // Todo 삭제
         @DeleteMapping("/{id}")
-        public void deleteTodo(
-                @PathVariable Integer id,
-                @RequestHeader("Authorization") String authorizationHeader
-        ) {
-                String accessToken = authorizationHeader.replace("Bearer ", "").trim();
-                String email = tokenStore.getEmailByToken(accessToken);
+        public void deleteTodo(@PathVariable Integer id, @RequestHeader("Authorization") String authorizationHeader) {
+//                String accessToken = authorizationHeader.replace("Bearer ", "").trim();
+//                String email = tokenStore.getEmailByToken(accessToken);
+                String token = authorizationHeader.replace("Bearer ","").trim();
+                String email = jwtTokenProvider.extractEmail(token);
+
 
                 todoService.deleteTodo(id, email);
         }
 
-        // Todo 수정 (나중에 구현)
+        // Todo 수정
+
+
 }
