56031fe9bfe6ec7ded73e2f3b19d161ddc1001cb
HyeonJun Kang <khjj9865@naver.com>
2025-11-19 20:39:30 +0900

    feat: S3 추가

---
+++ src/main/java/com/teamgreen/makeplan/server/repository/S3Repository.java
@@ -0,0 +1,41 @@
+package com.teamgreen.makeplan.server.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Repository
+@RequiredArgsConstructor
+public class S3Repository {
+
+    private final S3Client s3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String upload(MultipartFile file, String dir)  throws IOException {
+        String fileName = dir + "/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
+
+        PutObjectRequest putObjectRequest = PutObjectRequest
+                .builder()
+                .bucket(bucket)
+                .key(fileName)
+                .contentType(file.getContentType())
+                .acl("public-read") // S3버킷이 퍼블릭 read 접근을 허용하도록
+                .build();
+
+        s3Client.putObject(
+                putObjectRequest,
+                RequestBody.fromBytes(file.getBytes())
+        );
+
+        return "https://" + bucket + ".s3.amazonaws.com/" + fileName;
+    }
+}
